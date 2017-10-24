106cd02b081d18de4e211e2acb029b244f0b2629
Ong <865208597@qq.com>
2017-10-24 16:37:48 +0800

    添加最初的httpsUtils

--- transmission/src/main/java/ong/transmission/App.java
+++ transmission/src/main/java/ong/transmission/App.java
@@ -1,5 +1,10 @@
 package ong.transmission;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ong.http.helpers.HttpsUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Map<String,Object> context = new HashMap<String,Object>();
+    	
+    	Map<String, Object> params = new HashMap<String, Object>();
+    	params.put("a", "1");
+    	params.put("b", "2");
+    	if(HttpsUtils.getParams("http://127.0.0.1:8001/test", params, context)){
+    		
+    	}
+       
     }
 }
